d5023896ad2ae5a0bb5c949fe4b51f2831851eb2
debyaz <yahyachehimi47@gmail.com>
2024-05-03 00:10:20 +0200

    adding coordinate system

--- main.py
+++ main.py
@@ -2,6 +2,18 @@ import cv2 as cv
 import string
 import numpy as np
 
+f = open('coordinates.txt', 'w')
+def calculate_coordinates(contmatrix):
+    f.write('\n----------------------frame----------------------------\n')
+    for i in range(0,len(contmatrix[:][:])):
+        for k in range(0,len(contmatrix[i][:])):
+            t = contmatrix[i][k]
+            x = t[0][0]
+            y = t[0][1]
+            line = '('+str(x)+','+str(y)+',0),'
+            
+            f.write(line)
+            #print('(',x,',',y,',',0,'),')
 cv.namedWindow('MRI', cv.WINDOW_NORMAL)
 cv.resizeWindow('MRI',512,512)
 num:int = 1
@@ -9,7 +21,7 @@ num:int = 1
 directory:string = 'Series-80341/'
 file:string = directory + str(num) + '.jpg'
 image = cv.imread(file)
-cv.imshow('MRI',image)
+#cv.imshow('MRI',image)
 
 for y in range(0,1):
     num = 1
@@ -22,15 +34,15 @@ for y in range(0,1):
         contours, heir = cv.findContours(thresh,cv.RETR_LIST,cv.CHAIN_APPROX_SIMPLE)
         blank = np.zeros(image.shape[:],dtype='uint8')
         cv.drawContours(blank,contours,-1,(0,255,0) )
-        cv.imshow('contour',gray)
-        cv.imshow('vergin',blank)
-        cv.imshow('MRI',thresh)
+        #cv.imshow('contour',gray)
+        #cv.imshow('vergin',blank)
+        #cv.imshow('MRI',thresh)
+        calculate_coordinates(contours)
         num += 1
-        cv.waitKey(100)
         
 
 
 
-
+f.close()
 #cv.waitKey(0)
 cv.destroyAllWindows()
