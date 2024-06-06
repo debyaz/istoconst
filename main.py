d56d97a5b60869d7af3ad8f6f3c19b653b510c4b
debyaz <yahyachehimi47@gmail.com>
2024-06-06 20:25:41 +0200

    creating vertecies

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import cv2 as cv
 import string
 import numpy as np
+import bpy
 
-f = open('coordinates.txt', 'w')
+#f = open('coordinates.txt', 'w')
 def calculate_coordinates(contmatrix, layer:float):
-    f.write('\n----------------------frame----------------------------\n')
+    verts = []
     for i in range(0,len(contmatrix[:][:])):
         for k in range(0,len(contmatrix[i][:])):
             t = contmatrix[i][k]
-            x = t[0][0]
-            y = t[0][1]
-            line = '('+str(x)+','+str(y)+', '+ str(layer)+'),'
-            
-            f.write(line)
-            #print('(',x,',',y,',',0,'),')
+            #x = t[0][0]
+            #y = t[0][1]
+            coordinates = (t[0][0],t[0][1],layer)
+            verts.append(coordinates)
+
+    return verts
 cv.namedWindow('MRI', cv.WINDOW_NORMAL)
 cv.resizeWindow('MRI',512,512)
 num:int = 1
@@ -22,7 +23,7 @@ directory:string = 'Series-80341/'
 file:string = directory + str(num) + '.jpg'
 image = cv.imread(file)
 #cv.imshow('MRI',image)
-offsetValue:float = 0.5
+offsetValue:float = 5
 for y in range(0,1):
     num = 1
     for x in range(0,90):
@@ -33,17 +34,27 @@ for y in range(0,1):
         a, thresh = cv.threshold(gray,75,255,cv.THRESH_BINARY)
         contours, heir = cv.findContours(thresh,cv.RETR_LIST,cv.CHAIN_APPROX_SIMPLE)
         blank = np.zeros(image.shape[:],dtype='uint8')
-        cv.drawContours(blank,contours,-1,(0,255,0) )
+        #cv.drawContours(blank,contours,-1,(0,255,0) )
         #cv.imshow('contour',gray)
         #cv.imshow('vergin',blank)
         #cv.imshow('MRI',thresh)
-        calculate_coordinates(contours,num*offsetValue)
+        
+        #blender open
+        vertex = calculate_coordinates(contours,num*offsetValue)
+        faces = []
+        edges = []
+        mesh_data = bpy.data.meshes.new("cube_data")
+        mesh_data.from_pydata(vertex, edges, faces)
+        mesh_obj = bpy.data.objects.new("cube_object", mesh_data)
+        bpy.context.collection.objects.link(mesh_obj)
+        #blender close
         num += 1
         
         
 
 
 
-f.close()
+#f.close()
 #cv.waitKey(0)
+#print(len(calculate_coordinates(contours,num*offsetValue)))
 cv.destroyAllWindows()
